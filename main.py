afeed44b854b19f4b20dd74f4045f35944662dbc
MH_ProDev <MHProDev2020@gmail.com>
2022-04-02 00:37:07 +0430

    to async

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
-from pystyle import Colorate, Colors, Center
-from contextlib import suppress
-from socket import gethostname
+import asyncio
 from os import system, name
+from socket import gethostname
+
+from aioconsole import aprint, ainput as _input
+from pystyle import Colorate, Colors, Center
 
 
+class Console:
+    @staticmethod
+    async def banner():
+        await Console.clear()
+        await aprint(Colorate.Diagonal(Colors.yellow_to_red, Center.XCenter("""
+                                ╔════════════════════════════════════════════════════════════════════════════════╗
+                                ║    ooooooooo.              ooooooooooooo                     oooo              ║
+                                ║    `888   `Y88.             8'   888   `8                     `888             ║
+                                ║     888   .d88' oooo    ooo      888       .ooooo.   .ooooo.   888   .oooo.o   ║
+                                ║     888ooo88P'   `88.  .8'       888      d88' `88b d88' `88b  888  d88(  "8   ║
+                                ║     888           `88..8'        888      888   888 888   888  888  `"Y88b.    ║
+                                ║     888            `888'         888      888   888 888   888  888  o.  )88b   ║
+                                ║    o888o            .8'         o888o     `Y8bod8P' `Y8bod8P' o888o 8""888P'   ║
+                                ║                 .o..P'                                                         ║
+                                ║                 `Y8P'                                                          ║
+                                ╚═══════════════════════╦═════════════════════════════════╦══════════════════════╝
+                                                        ║   Coded By : MXTeam - V 1.0     ║
+                                                        ╚═════════════════════════════════╝\n\n\n\n""")))
 
-def clear():
-    system('cls' if name == 'nt' else 'clear')
+    @staticmethod
+    async def input(*messages):
+        return await _input(''.join([txt for txt in messages]))
 
+    @staticmethod
+    async def clear():
+        system('cls' if name == 'nt' else 'clear')
 
-def Header():
-    clear()
-    print(Colorate.Diagonal(Colors.yellow_to_red, Center.XCenter("""
-                            ╔════════════════════════════════════════════════════════════════════════════════╗
-                            ║    ooooooooo.              ooooooooooooo                     oooo              ║
-                            ║    `888   `Y88.             8'   888   `8                     `888             ║
-                            ║     888   .d88' oooo    ooo      888       .ooooo.   .ooooo.   888   .oooo.o   ║
-                            ║     888ooo88P'   `88.  .8'       888      d88' `88b d88' `88b  888  d88(  "8   ║
-                            ║     888           `88..8'        888      888   888 888   888  888  `"Y88b.    ║
-                            ║     888            `888'         888      888   888 888   888  888  o.  )88b   ║
-                            ║    o888o            .8'         o888o     `Y8bod8P' `Y8bod8P' o888o 8""888P'   ║
-                            ║                 .o..P'                                                         ║
-                            ║                 `Y8P'                                                          ║
-                            ╚═══════════════════════╦═════════════════════════════════╦══════════════════════╝
-                                                    ║   Coded By : SudoLite - V 1.0   ║
-                                                    ╚═════════════════════════════════╝\n\n\n\n""")))
+    @staticmethod
+    async def run():
+        await Console.banner()
+        while 1:
+            inp = (await Console.input(Colorate.Horizontal(Colors.yellow_to_red,
+                                                           f"╔═══[{gethostname()}"
+                                                           f"@PyTools]\n╚══════> "))).strip()
+            if not inp: pass
+            await Console.handle(inp)
 
+    @staticmethod
+    async def handle(inp):
+        cmd, args = (inp + " ").split(" ", 1)
+        cmd = cmd.upper()
+        args = args or []
 
-def inputstyle(text=None):
-    return input(Colorate.Horizontal(Colors.yellow_to_red, f"╔═══[{gethostname()}{text}@PyTools]\n╚══════> "))
-
+        if {cmd} & {"HELP", "?"}:
+            await aprint("help ?")
 
 
 if __name__ == '__main__':
-    Header()
-    cons = f"╔═══[{gethostname()}@PyTools]\n╚══════ >"
-    with suppress(KeyboardInterrupt):
-        with suppress(IndexError):
-            while 1:
-                cmd = inputstyle("test").strip()
-                if not cmd: continue
-                if " " in cmd:
-                    cmd, args = cmd.split(" ", 1)
-
-                cmd = cmd.upper()
-                if cmd == "HELP":
-                    print("Commands: HELP, CLEAR, BACK, EXIT")
-                    continue
+    asyncio.run(Console.run())
